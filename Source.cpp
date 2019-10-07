f6f69121f37d9f8bbe33c4323c548670c4ffc07c
volovax <pyps9874@gmail.com>
2019-10-07 19:56:25 +0200

    First adding that lab, but i tired so changes aren't global

--- Source.cpp
+++ Source.cpp
@@ -1,37 +1,60 @@
+/*что такое виртуальный деструктор?
+производный и базовый класс?
+ЛАБА #3:
+реализовать абстрактный класс "фигура" ("shape").
+в классе буду объявлены абстрактные функции.
+от этого класса наследовать абстрактные классы "2d shape" и "3d shape"
+от "2d shape" наследуется классы "triangle" and "square"
+от "3d shape" наследуется классы "sphere" and "cyllinder".
+В фигурах в функция заносится 1 аргумент. Пример: int n в функции "2d shape".
+Создать конструктор с параметрами.
+Реализовать виртуальные функции print(), кот будут выводить название фигуры,
+volume() - вычисление объема фигуры, area() - площадь.
+Для каждого класса свой хедер.*/
 #include <iostream>
 
-class Device {	
+class Shape {	
 public:
-	Device() {
-		std::cout << "Device constructor called" << std::endl;
-	}
-	~Device() {
-		std::cout << "Device destructor called" << std::endl;
-	}
+	void virtual print() = 0;
 };
 
-class Computer {
+class _2dShape: public Shape {
 public:
-	Computer() {
-		std::cout << "Computer constructor called" << std::endl;
-	}
-	~Computer() {
-		std::cout << "Computer destructor called" << std::endl;
-	}
+	int width = 0;
 };
 
-class Laptop : public Computer, public Device {
+class _3dShape : public Shape {
 public:
-	Laptop() {
-		std::cout << "Laptop constructor called" << std::endl;
-	}
-	~Laptop() {
-		std::cout << "Laptop destructor called" << std::endl;
-	}
+	int high = 0;
 };
 
+class Triangle : public _2dShape {
+	
+public:
+	int side1 = 0;
+	int side2 = 0;
+	int side3 = 0;
+	Triangle(int first, int second, int third) {
+		side1 = first;
+		side2 = second;
+		side3 = third;
+	}
+	//	next will be Area of Triangle, but I tired 
+};
+
+class Square : public _2dShape {
+
+};
+
+class Sphere : public _3dShape {
+	
+};
+
+
+class Cyllinder : public _3dShape {
+
+};
 int main() {
-	Laptop Laptop_instance;
 
 	return 0;
 }
